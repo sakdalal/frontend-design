1bb589a5f1319d16dda12e2edeb0080539d42bed
sak <sakshi19dalal@gmail.com>
2026-03-15 20:45:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// simple form message
+
+// let form = document.getElementById("form")
+
+// form.addEventListener("submit", function(e){
+
+// e.preventDefault()
+
+// alert("Thanks! We will contact you soon.")
+
+// })
+
+document.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert("Thank you! We will contact you soon.");
+
+});
